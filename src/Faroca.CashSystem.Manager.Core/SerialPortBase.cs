a60b4e02edfa181831eca290c7d915116f35b472
Francisco Aroca <francisco-80@hotmail.es>
2025-02-20 20:30:19 +0100

    added core project init

---
+++ src/Faroca.CashSystem.Manager.Core/SerialPortBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Emit;
+
+namespace Faroca.CashSystem.Manager.Core
+{
+    public abstract class SerialPortBase
+    {
+
+        public abstract bool IsCompleted(byte[] buffer);
+
+        public abstract int GetExpectedLength(byte[] buffer);
+    }
+}
